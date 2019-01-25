18d09af0470f6ae1ff547a82e2f1c92d7cbed1ba
joe kim <j80n89@gmail.com>
2019-01-25 14:56:21 -0500

    model 1 update good to go

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -12,6 +12,17 @@ const projectController = {
       .then(project => {
         res.send(project);
       });
+  },
+  create: (req, res) => {
+    Project.create(req.body).then(project => {
+      res.send(project);
+    });
+  },
+  update: (req, res) => {
+    Project.findByIdAndUpdate(req.params.projectId, req.body).then(project => {
+      project.save();
+      res.send(project);
+    });
   }
 };
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const projectController = require("../controllers/projectController");
 
 router.get("/api/projects", projectController.index);
+router.post("/api/projects", projectController.create);
 router.get("/api/projects/:projectId", projectController.show);
+router.put("/api/projects/:projectId", projectController.update);
 
 module.exports = router;
